80af9aa4ae1fa22ae585198ec594f5c5b48fe3ad
mrdsx <andre0.castlegames@gmail.com>
2026-03-02 19:35:33 +0300

    add alert dialog when can continue game

--- src/lib/components/StartGameForm.svelte
+++ src/lib/components/StartGameForm.svelte
@@ -1,9 +1,22 @@
 <script lang="ts" module>
+  import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "$lib/components/ui/alert-dialog";
   import { Label } from "$lib/components/ui/label";
   import { startNewWordGame, startWordGame } from "../../store/gameState";
   import { words } from "../../store/words";
   import MaxAttemptsNativeSelect from "./MaxAttemptsNativeSelect.svelte";
-  import { Button } from "./ui/button";
+  import { Button, buttonVariants } from "./ui/button";
+
+  const NEW_GAME_BUTTON_TEXT = "New game";
 </script>
 
 <script lang="ts">
@@ -34,6 +47,31 @@
     >
       Continue
     </Button>
-    <Button type="submit">New game</Button>
+    {#if canContinueGame}
+      <AlertDialog>
+        <AlertDialogTrigger
+          class={buttonVariants({ variant: "default" })}
+          type="button"
+        >
+          {NEW_GAME_BUTTON_TEXT}
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              You still have game in progress. It'll be lost.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>No</AlertDialogCancel>
+            <AlertDialogAction onclick={startNewWordGame}>
+              Yes
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    {:else}
+      <Button type="submit">{NEW_GAME_BUTTON_TEXT}</Button>
+    {/if}
   </div>
 </form>
