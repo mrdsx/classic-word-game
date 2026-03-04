04450e27bb119967b6211f7bc70ed2d9dc082ded
mrdsx <andre0.castlegames@gmail.com>
2026-03-04 21:37:46 +0300

    update frontend url for production environment

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,7 @@
-export const FRONTEND_URL = "http://localhost:3000";
+export const FRONTEND_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : "https://classic-word-game.netlify.app";
 
 export const MIN_WORD_LENGTH = 2;
 export const MAX_WORD_LENGTH = 30;
